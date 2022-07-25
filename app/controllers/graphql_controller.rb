1d0acc766f9ae0a50f37ba67f404f9efde6bfff4
SergPG <serg-popov@ukr.net>
2022-07-26 01:35:07 +0300

    improved request

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -3,6 +3,7 @@ class GraphqlController < ApplicationController
   # This allows for outside API access while preventing CSRF attacks,
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
+   protect_from_forgery with: :null_session
 
   def execute
     variables = prepare_variables(params[:variables])
@@ -12,6 +13,7 @@ class GraphqlController < ApplicationController
       # Query context goes here, for example:
       # current_user: current_user,
     }
+
     result = GithabProjectSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
   rescue StandardError => e
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,12 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :github, Types::GhithubObjectType, null: false do
+      argument :login, String, required: true
+    end
+
+    def github(login:)
+      GithubService.new(login).call
     end
   end
 end
